ede0cccd82d6a096ebb4a60027a6f72bce715dd0
samirm00 <sammoham.be@gmail.com>
2024-06-05 19:58:49 +0200

    add hello-world

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,7 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+});
+
 import './App.css';
 
 const App = () => {
-    return <div>Hello, World!</div>;
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Typography variant="h2">Hello, World!</Typography>
+        </ThemeProvider>
+    );
 };
 
 export default App;
